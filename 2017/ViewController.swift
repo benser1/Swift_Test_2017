969d172724fa3de619353bf68f078a291296197b
benser1 <briganenser28@gmail.com>
2017-01-14 09:39:51 -0500

    First commit. 2 text fields to add numbers together working.

--- 2017/ViewController.swift
+++ 2017/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var greetingLabel: UILabel!
 
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        
+        greetingLabel.text = "Answer \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
